c16f6371099b3828fe23c6ab5a4066d526a01fd1
Samuel <carlomagne3@gmail.com>
2025-06-03 18:43:19 +0200

    Aplication properties changed

--- src/main/java/app/healthy/bot/config/WebConfig.java
+++ src/main/java/app/healthy/bot/config/WebConfig.java
@@ -4,14 +4,18 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:8080")  // specify allowed origins
-                .allowedMethods("GET", "POST", "PUT", "DELETE")
+                .allowedOrigins("*")  // Permite acceso desde cualquier origen (puedes restringirlo luego)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowCredentials(true);
     }
 }
